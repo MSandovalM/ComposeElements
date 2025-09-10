0c154a1f2f6857ea67b134b6df8ec3a5bb607273
Manuel Sandoval Mendoza <msandovalm01@gmail.com>
2025-09-10 12:21:52 -0600

    Added TopAppBar

--- app/src/main/java/com/jetpackmsm/mylearningappone/MainActivity.kt
+++ app/src/main/java/com/jetpackmsm/mylearningappone/MainActivity.kt
@@ -4,14 +4,19 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import com.jetpackmsm.mylearningappone.components.MyProgressBar
+import com.jetpackmsm.mylearningappone.components.MyTopAppBar
 import com.jetpackmsm.mylearningappone.ui.theme.MyLearningAppOneTheme
 
 class MainActivity : ComponentActivity() {
@@ -20,8 +25,20 @@ class MainActivity : ComponentActivity() {
         enableEdgeToEdge()
         setContent {
             MyLearningAppOneTheme {
-                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
-                    MyProgressBar(Modifier.padding(innerPadding))
+                Scaffold(
+                    modifier = Modifier.fillMaxSize(),
+                    topBar = {
+                        MyTopAppBar()
+                    }) { innerPadding ->
+                    Box(
+                        modifier = Modifier
+                            .fillMaxSize()
+                            .background(Color.Cyan)
+                            .padding(innerPadding),
+                        contentAlignment = Alignment.Center
+                    ) {
+                        Text("Esta es mi Screen")
+                    }
                 }
             }
         }
